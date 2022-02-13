c9a81b7613820cb451acf4fc33d489f87d35bedb
KR <kyranrawlinson@gmail.com>
2022-02-13 21:28:00 +0000

    Added unit test: test_get_user_total_balance_with_different_tokens_and_amounts

--- tests/unit/test_token_farm.py
+++ tests/unit/test_token_farm.py
@@ -100,3 +100,20 @@ def test_unstake_tokens(amount_staked):
     assert dapp_token.balanceOf(account.address) == KEPT_BALANCE
     assert token_farm.stakingBalance(dapp_token.address, account.address) == 0
     assert token_farm.uniqueTokensStaked(account.address) == 0
+
+def test_get_user_total_balance_with_different_tokens_and_amounts(amount_staked, random_erc20):
+    # Arrange
+    if not config['networks'][network.show_active()]['local'] is True:
+        pytest.skip('Only tested on local networks')
+    account = get_account()
+    token_farm, dapp_token = test_stake_tokens(amount_staked)
+    #Act
+    token_farm.addAllowedTokens(random_erc20.address, {"from": account})
+    token_farm.setPriceFeedContract(random_erc20.address, get_contract("eth_usd_price_feed"), {"from": account})
+    random_erc20_stake_amount = amount_staked * 2
+    random_erc20.approve(token_farm.address, random_erc20_stake_amount, {"from": account})
+    token_farm.stakeTokens(random_erc20_stake_amount, random_erc20.address, {"from": account})
+    #Assert
+    total_eth_balance = token_farm.getUserTotalValue(account.address)
+    print (total_eth_balance)
+    assert total_eth_balance == INITIAL_PRICE_FEED_VALUE * 3
