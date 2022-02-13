78b95224ac9464d824aaeab3ca6d6c0222502d8e
KR <kyranrawlinson@gmail.com>
2022-02-13 17:26:25 +0000

    Created and setup tests/unit/test_token_farm.py

---
+++ tests/unit/test_token_farm.py
@@ -0,0 +1,35 @@
+from scripts.general_scripts import get_account, get_contract
+from scripts.deploy import deploy_token_farm_and_dapp_token
+from brownie import config, network
+import pytest
+
+def test_set_price_feed_contract():
+    # Arrange
+    if not config['networks'][network.show_active()]['local'] is True:
+        pytest.skip('Only tested on local networkss')
+    account = get_account()
+    non_owner = get_account(index=1)
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+    # Act
+    token_farm.setPriceFeedContract(dapp_token.address, get_contract("eth_usd_price_feed"), {"from": account}).wait(1)
+    # Assert
+    assert token_farm.tokenPriceFeedMapping(dapp_token.address) == get_contract("eth_usd_price_feed")
+    with pytest.raises(AttributeError):
+        token_farm.setPriceFeedContract(dapp_token.address, get_contract("eth_usd_price_feed"), {"from": non_owner})
+
+def test_stake_tokens(amount_staked):
+    # Arrange
+    if not config['networks'][network.show_active()]['local'] is True:
+        pytest.skip('Only tested on local networkss')
+    account = get_account()
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+    # Act
+    dapp_token.approve(token_farm.address, amount_staked, {"from": account})
+    token_farm.stakeTokens(amount_staked, dapp_token.address, {"from": account}).wait(1)
+    # Assert
+    assert (token_farm.stakingBalance(dapp_token.address, account.address) == amount_staked)
+    assert token_farm.uniqueTokensStaked(account.address) == 1
+    assert token_farm.stakers(0) == account.address
+
+    return token_farm, dapp_token
+    
