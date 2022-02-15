c16fbc0f16e8c965faa630d9f2fe0adb1b11f78f
KR <kyranrawlinson@gmail.com>
2022-02-15 15:51:25 +0000

    Added script to update front end: scripts/update_front_end.py

---
+++ scripts/update_front_end.py
@@ -0,0 +1,12 @@
+import yaml, json
+
+def update_front_end():
+    # Sending the front end; brownie config in JSON format
+    with open("brownie-config.yaml", "r") as brownie_config:
+        config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
+        with open("./front_end/src/brownie-config.json", "w") as brownie_config_json:
+            json.dump(config_dict, brownie_config_json)
+    print('Front end updated ...\n')
+
+def main():
+    update_front_end()
