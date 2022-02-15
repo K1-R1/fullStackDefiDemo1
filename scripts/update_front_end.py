cdb6fcda518be0b3d1d4ee7e8c0ec7b680e4716e
KR <kyranrawlinson@gmail.com>
2022-02-15 16:00:35 +0000

    Updated scripts/update_front_end.py; with functionality to copy build folder to frontend

--- scripts/update_front_end.py
+++ scripts/update_front_end.py
@@ -1,13 +1,20 @@
-import yaml, json
+import yaml, json, os, shutil
 
 def main():
     update_front_end()
 
 def update_front_end():
+    # Sending the front end; build folder
+    copy_folders_to_front_end("./build", "./front_end/src/brownie-build")
+
     # Sending the front end; brownie config in JSON format
     with open("brownie-config.yaml", "r") as brownie_config:
         config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
         with open("./front_end/src/brownie-config.json", "w") as brownie_config_json:
             json.dump(config_dict, brownie_config_json)
     print('Front end updated ...\n')
-    
+
+def copy_folders_to_front_end(src, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    shutil.copytree(src, dest)
