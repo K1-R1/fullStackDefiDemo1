cb6093f3697647d8e2235d82791d64cf5c66eaab
KR <kyranrawlinson@gmail.com>
2022-02-15 21:09:31 +0000

    Updated scripts/update_front_end.py

--- scripts/update_front_end.py
+++ scripts/update_front_end.py
@@ -4,17 +4,37 @@ def main():
     update_front_end()
 
 def update_front_end():
-    # Sending the front end; build folder
-    copy_folders_to_front_end("./build", "./front_end/src/brownie-build")
+    # The Build
+    copy_folders_to_front_end("./build/contracts", "./front_end/src/brownie-build")
 
-    # Sending the front end; brownie config in JSON format
+    # The Contracts
+    copy_folders_to_front_end("./contracts", "./front_end/src/contracts")
+
+    # The ERC20
+    copy_files_to_front_end(
+        "./build/contracts/dependencies/OpenZeppelin/openzeppelin-contracts@4.5.0/ERC20.json",
+        "./front_end/src/brownie-build/ERC20.json",
+    )
+    # The Map
+    copy_files_to_front_end(
+        "./build/deployments/map.json",
+        "./front_end/src/brownie-build/map.json",
+    )
+
+    # The Config, converted from YAML to JSON
     with open("brownie-config.yaml", "r") as brownie_config:
         config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
-        with open("./front_end/src/brownie-config.json", "w") as brownie_config_json:
+        with open(
+            "./front_end/src/brownie-config.json", "w"
+        ) as brownie_config_json:
             json.dump(config_dict, brownie_config_json)
-    print('Front end updated ...\n')
 
 def copy_folders_to_front_end(src, dest):
     if os.path.exists(dest):
         shutil.rmtree(dest)
     shutil.copytree(src, dest)
+
+def copy_files_to_front_end(src, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    shutil.copyfile(src, dest)
